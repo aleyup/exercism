94f8656eaf080714021f5c7f1b4933c2fdda2a3f
aleyup <celayagizn@gmail.com>
2021-04-30 03:28:17 +0300

    added resistor-color exercise

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,21 @@
+const colors = {
+ Black: 0,
+ Brown: 1,
+ Red: 2,
+ Orange: 3,
+ Yellow: 4,
+ Green: 5,
+ Blue: 6,
+ Violet: 7,
+ Grey: 8,
+ White: 9,
+}
+
+export const colorCode = (color) => {
+  for (let i of Object.entries(colors).map(elem => [elem[0].toLocaleLowerCase(), elem[1]])) {
+    if(i.includes(color)) return i[1]
+  }
+}; //getting an Array of Objects "colors" properties and changing the case of colors keys
+//then iterating through that array with for..of loop and returning the colors corresponding numbers
+
+export const COLORS = Object.keys(colors).map(elem => elem.toLocaleLowerCase());
--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -19,29 +19,29 @@ export const encode = (string) => {
   return encoded.join('')
 };
 
+// still second function to finish. Think about cutting the first one (with regEx possibly)
 
+// export const decode = (string) => {
+//   let arr = Array.from(string)
+//   console.log(arr)
+//   let str = [];
 
-export const decode = (string) => {
-  let arr = Array.from(string)
-  console.log(arr)
-  let str = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let cur = arr[i]
+//     let prev = arr[i - 1];
+//     let next = arr[i + 1]
+//     if (isFinite(prev)) {
+//       let num = Number(prev)
+//       str.push(arr[i].repeat(num))
+//     } else if(isFinite(next)) {
+//       str.push(arr[i])
+//     } else if(!isFinite(cur) && !isFinite(prev) && !isFinite(next)) {
+//       str.push(cur)
+//     }
+//   }
+//   // return str.join('')
+//   console.log(str.join(''))
+// };
 
-  for (let i = 0; i < arr.length; i++) {
-    let cur = arr[i]
-    let prev = arr[i - 1];
-    let next = arr[i + 1]
-    if (isFinite(prev)) {
-      let num = Number(prev)
-      str.push(arr[i].repeat(num))
-    } else if(isFinite(next)) {
-      str.push(arr[i])
-    } else if(!isFinite(cur) && !isFinite(prev) && !isFinite(next)) {
-      str.push(cur)
-    }
-  }
-  // return str.join('')
-  console.log(str.join(''))
-};
-
-encode('bbbbbbbbbbbbbbbbbbcccc')
-decode('2  hs12q q2w2 ')
+// encode('bbbbbbbbbbbbbbbbbbcccc')
+// decode('2  hs12q q2w2 ')
